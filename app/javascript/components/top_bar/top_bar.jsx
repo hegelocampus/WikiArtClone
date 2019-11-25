d54d66aa31ac44607625fc033f67f620b7b87dbb
Bee <pizzaman8099@gmail.com>
2019-11-25 12:48:22 -0800

    Make logo image link to root

--- app/javascript/components/top_bar/top_bar.jsx
+++ app/javascript/components/top_bar/top_bar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import AuthModal from '../auth_modal/auth_modal';
 import UserMenu from './top_bar_user_menu';
 import EditMenu from './top_bar_edit_artist';
@@ -21,7 +22,9 @@ export default (props) => {
     <header>
       <nav className="top-nav">
         <div className="top-bar-logo-container">
-          <img src={ window.logoURL } alt="WikiArt logo" className="top-bar-logo" />
+          <NavLink to="/">
+            <img src={ window.logoURL } alt="WikiArt logo" className="top-bar-logo" />
+          </NavLink>
         </div>
         <div className="top-bar-side-nav">
           { authContent }

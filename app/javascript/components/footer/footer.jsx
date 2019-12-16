25ba948bf09262c429bf27b62e4bde8890007677
Bee <pizzaman8099@gmail.com>
2019-12-15 22:08:05 -0800

    Add attribution and better styling to footer

--- app/javascript/components/footer/footer.jsx
+++ app/javascript/components/footer/footer.jsx
@@ -5,6 +5,12 @@ import { faGithub } from '@fortawesome/free-brands-svg-icons';
 
 export default () => (
   <footer className="footer">
+    <div className="attribution">
+      <span><small>
+        All data in this application is liscensed under a Creative Commons Zero (CC0) 1.0 designation.
+        All art images are retreived from the <a href="https://aggregator-data.artic.edu/home">Art Institute of Chicago API.</a> If you like the artworks you see here, please support their institution and the great work they do for the open source art community.
+      </small></span>
+    </div>
     <ul className="social-link-list">
       <li className="list-inline-item">
         <a className="social-link" href="https://bellis.dev">

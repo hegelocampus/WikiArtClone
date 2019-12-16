f35d8f6d871e921e0fc605db8221499772398e4a
Bee <pizzaman8099@gmail.com>
2019-12-15 22:06:13 -0800

    Change artist wiki to use html from api

--- app/javascript/components/artist/artist_wiki.jsx
+++ app/javascript/components/artist/artist_wiki.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import ReactHtmlParser from 'react-html-parser'
 import { fetchWiki } from '../../actions/wiki_actions';
 
 export default ({ artistWiki }) => {
@@ -20,7 +21,9 @@ export default ({ artistWiki }) => {
       {wikiText ? (
         <div className="artist-wiki">
           <h4>Wikipedia article</h4>
-          <p>{ wikiText }</p>
+          <div className="wiki-excerpt">
+            { ReactHtmlParser (wikiText) }
+          </div>
         </div>
       ) : (
         null
--- app/javascript/utils/wiki_api_util.js
+++ app/javascript/utils/wiki_api_util.js
@@ -1,7 +1,7 @@
 export const fetchWikiIntro = url => {
   let parsed = url.match(/\/wiki\/(.*)/)[1];
   return $.ajax({
-    url: `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&explaintext&redirects=1&orgin=*&format=json&titles=${ parsed }`,
+    url: `https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exintro&exlimit=2&redirects=2&orgin=*&format=json&titles=${ parsed }`,
     dataType: 'jsonp',
     headers: { 'Api-User-Agent': "benellis8099@gmail.com" },
     data: {
